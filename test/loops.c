2a75b0058641fbf2f25f73f3c24809d2eb7ff1d7
Leo Kuznetsov <leo.kuznetsov@gmail.com>
2026-02-18 02:33:40 -0800

    test/loops.c

---
+++ test/loops.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    printf("--- for loop (existing var) ---
+");
+    for (i = 0; i < 5; i = i + 1) {
+        if (i == 1) {
+            continue;
+        }
+        if (i == 4) {
+            break;
+        }
+        printf("i = %d
+", i);
+    }
+
+    printf("--- for loop (scoped int j) ---
+");
+    for (int j = 0; j < 5; j = j + 1) {
+        if (j == 1) {
+            continue;
+        }
+        if (j == 4) {
+            break;
+        }
+        printf("j = %d
+", j);
+    }
+
+    printf("--- while loop ---
+");
+    i = 0;
+    while (i < 5) {
+        if (i == 1) {
+            i = i + 1;
+            continue;
+        }
+        if (i == 4) {
+            break;
+        }
+        printf("i = %d
+", i);
+        i = i + 1;
+    }
+
+    printf("--- do-while loop ---
+");
+    i = 0;
+    do {
+        if (i == 1) {
+            i = i + 1;
+            continue;
+        }
+        if (i == 4) {
+            break;
+        }
+        printf("i = %d
+", i);
+        i = i + 1;
+    } while (i < 5);
+
+    return 0;
+}
